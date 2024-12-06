40a4b2d3e89db731e93276519a9dbba93cff63af
Adam Rzemiński <116032074+AdamRzem@users.noreply.github.com>
2024-12-06 18:11:39 +0100

    second

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,29 @@
 <script lang="ts">
 	import '../app.css';
+	import Map from './components/Map.svelte';
+	import Navbar from './components/Navbar.svelte';
 	let { children } = $props();
 </script>
 
-{@render children()}
+<Navbar />
+
+<style>
+	:global(html) {
+		transition: background-color 0.3s, color 0.3s;
+	}
+	:global(html.dark) {
+		background-color: #1a202c;
+		color: #a0aec0;
+	}
+	:global(html.light) {
+		background-color: #f7fafc;
+		color: #2d3748;
+	}
+</style>
+
+{@render children()} 
+
+
+	<footer class="bg-slate-100 dark:bg-gray-800 p-4 text-white text-center relative bottom-0 mt-2.5">
+		<Map />
+	</footer>
---
+++ src/routes/components/Map.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+
+    let map;
+
+    onMount(() => {
+        // Load the Google Maps script
+        const script = document.createElement('script');
+        script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDQ7LHfqJbOenpkqmduiUjhwNSL9L3Duhk`;
+        script.async = true;
+        script.defer = true;
+        script.onload = () => {
+            // Initialize the map
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: { lat: 49.610014, lng: 20.703700 }, // Coordinates for Nowy Sącz
+                zoom: 17
+            });
+        };
+        document.head.appendChild(script);
+    });
+</script>
+
+<style>
+    #map {
+        height: 50vh;
+        width: 50vh;
+        border-radius: 15px; /* Add this line to give rounded corners */
+    }
+</style>
+
+<div id="map"></div>
