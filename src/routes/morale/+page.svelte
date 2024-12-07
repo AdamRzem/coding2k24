c6bf5831465b47d62bd2d0464b4c6b7c64f9f37d
Z0si4 <michalbryla06@gmail.com>
2024-12-07 06:14:54 +0100

    Update +page.svelte

--- src/routes/morale/+page.svelte
+++ src/routes/morale/+page.svelte
@@ -1,15 +1,30 @@
 <script lang="ts">
-    import { onMount } from "svelte";
+    import { onMount, onDestroy } from "svelte";
     import type { PageServerData } from "./$types";
     import { Chart } from "flowbite-svelte";
+
+    let audio: HTMLAudioElement | null = null;
+
     onMount(() => {
-        const audio = new Audio("NagraniaCoding/Morale.mp3"); // Upewnij się, że ścieżka do pliku jest poprawna
+        // Inicjalizacja i odtwarzanie dźwięku
+        audio = new Audio("NagraniaCoding/Morale.mp3"); // Upewnij się, że ścieżka do pliku jest poprawna
         audio.play().catch((error) => {
             console.error("Audio playback failed:", error);
         });
     });
+
+    onDestroy(() => {
+        // Zatrzymywanie i resetowanie odtwarzania dźwięku
+        if (audio) {
+            audio.pause();
+            audio.currentTime = 0; // Reset pozycji odtwarzania
+            audio = null;
+        }
+    });
+
     let { data }: { data: PageServerData } = $props();
     console.log(data.resp);
+
     let group: ApexCharts.ApexOptions = {
         chart: {
             type: "bar",
@@ -28,7 +43,7 @@
             {
                 // name: "Morale",
                 data: data.resp.map((reading) => {
-                    return { x: reading.emotion??"null", y: reading.cnt };
+                    return { x: reading.emotion ?? "null", y: reading.cnt };
                 }),
             },
         ],
