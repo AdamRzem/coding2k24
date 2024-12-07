afc95e5c00c0d1d4d4c5a994571948adf3a920e2
Z0si4 <michalbryla06@gmail.com>
2024-12-07 05:10:06 +0100

    Dźwięki

--- src/routes/morale/+page.svelte
+++ src/routes/morale/+page.svelte
@@ -1,1 +1,9 @@
-<!--rozpoznawanie twarzy, info z nich, ocena morali, Julka-->
+<script lang="ts">
+    import { onMount } from "svelte";
+    onMount(() => {
+        const audio = new Audio('NagraniaCoding/Morale.mp3'); // Upewnij się, że ścieżka do pliku jest poprawna
+        audio.play().catch((error) => {
+            console.error("Audio playback failed:", error);
+        });
+    });
+</script>
