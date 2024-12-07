929bf57a6d3d3321c0b00cfe27f977e5348c9832
Z0si4 <michalbryla06@gmail.com>
2024-12-07 04:22:33 +0100

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,6 @@
-<video autoplay muted loop playsinline style="width: 100%; height: auto;" controlslist="nodownload nofullscreen noremoteplayback" disablePictureInPicture>
+<div style="display: flex; justify-content: center; align-items: center; height: 100vh; background-color: #000;">
+  <video autoplay muted loop playsinline style="width: 30%; height: auto;" controlslist="nodownload nofullscreen noremoteplayback" disablePictureInPicture>
     <source src="NagraniaCoding/About.mp4" type="video/mp4" />
     Your browser does not support the video tag.
   </video>
-  
+</div>
