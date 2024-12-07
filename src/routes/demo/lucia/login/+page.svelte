8007283447ef060cfa65006eedc57b768ab44dc5
Adam Rzemiński <116032074+AdamRzem@users.noreply.github.com>
2024-12-07 07:27:41 +0100

    style

--- src/routes/demo/lucia/login/+page.svelte
+++ src/routes/demo/lucia/login/+page.svelte
@@ -10,13 +10,13 @@
 	<div>
 		<label class="block text-sm font-medium light:text-gray-700 dark:text-white">
 			Username
-			<input name='username' class="mt-1 block w-50 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+			<input name='username' class="mt-1 block w-50 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-black" />
 		</label>
 	</div>
 	<div>
 		<label class="block text-sm font-medium light:text-gray-700 dark:text-white">
 			Password
-			<input type='password' name='password' class="mt-1 block w-50 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+			<input type='password' name='password' class="mt-1 block w-50 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-black" />
 		</label>
 	</div>
 	<div class="flex space-x-4">
