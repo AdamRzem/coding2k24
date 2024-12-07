80ddc23406326024b959b6cb043eac209f3df054
ProsESportu <t.hajduga@poczta.fm>
2024-12-07 03:02:23 +0100

    Add drizzle-zod for schema validation and implement POST route for morale data insertion

---
+++ src/routes/cam/+server.ts
@@ -0,0 +1,11 @@
+import { createInsertSchema } from 'drizzle-zod';
+import type { RequestHandler } from './$types';
+import * as table from '$lib/server/db/schema';
+import { z } from "zod";
+import { db } from '$lib/server/db';
+export const POST: RequestHandler = async (event) => {
+    const moraleSchema = createInsertSchema(table.morale);
+    const data = moraleSchema.parse(await event.request.json());
+    const res = await db.insert(table.morale).values(data).execute();
+    return new Response(JSON.stringify(res), { status: 200 });
+};
